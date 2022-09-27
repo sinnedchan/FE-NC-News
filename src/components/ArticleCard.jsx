6f684a12b9e25e6a84f5c3d4caa47ce66ad6b381
sinnedchan <dench0416@gmail.com>
2022-09-27 11:21:40 +0100

    implemented loading msg

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,7 +1,7 @@
 export default function ArticleCard({ article }) {
   return (
     <div>
-      <h2>{article.title}</h2>
+      <h3>{article.title}</h3>
       <p>
         {article["author_id"]} {article.topic} {article["comment_count"]}
         {article.votes}
--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,14 +4,19 @@ import ArticleCard from "./ArticleCard";
 
 export default function ArticleList() {
   const [articles, setArticles] = useState([]);
-
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     axios
       .get("https://sinnedchan-nc-new-example-api.herokuapp.com/api/articles")
       .then(({ data }) => {
         setArticles(data.articles);
+        setIsLoading(false);
       });
   }, []);
+
+  if (isLoading) {
+    return <h2> Articles Loading </h2>;
+  }
   return (
     <div>
       {articles.map((article) => (
